0da2efbe4e7fc43f0056cd84da836208717f440d
priyadeepthi422 <priyadeepthi422@gmail.com>
2020-11-13 23:39:53 +0530

    added reducer,container,action

--- src/containers/HomeContainer.js
+++ src/containers/HomeContainer.js
@@ -1,8 +1,16 @@
 import Home from '../components/Home';
+import { connect } from 'react-redux';
+import { addToCart } from '../Services/Actions/actions';
+
+const mapStateToProps = (state) => ({
+
+})
 
 
+const mapDispatchToProps = (dispatch) => ({
+    addToCartHandler: data => dispatch(addToCart(data))
 
+})
 
-
-
-export default Home;
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
+//export default Home;
